bf2a19c8d158973d38e9ebd073675b193537df4d
Kowenjko <ortoswt@gmail.com>
2025-11-27 10:55:31 +0200

    add sidebar in layout

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,18 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import { CenteredLayout } from '../layout'
+import { CenteredLayout, SidebarLayout } from '../layout'
 import { AUTH_SECTION_ROUTE } from '@/pages/auth'
 import { MAIN_ROUTE } from '@/pages/main'
+import { MAIN_LINK } from '@/shared/config'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       ...AUTH_SECTION_ROUTE,
-      component: CenteredLayout,
+      component: CenteredLayout
     },
-    MAIN_ROUTE,
-  ],
+    {
+      path: MAIN_ROUTE.path,
+      redirect: MAIN_LINK,
+      children: [
+        {
+          path: '',
+          name: MAIN_ROUTE.name,
+          component: MAIN_ROUTE.component
+        }
+      ],
+      component: SidebarLayout
+    }
+  ]
 })
 
 export default router
