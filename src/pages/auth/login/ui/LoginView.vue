0db78646cd2a9f469e2ee8e952a998b02b08722a
Kowenjko <ortoswt@gmail.com>
2025-11-27 10:32:31 +0200

    fix login and register

--- src/pages/auth/login/ui/LoginView.vue
+++ src/pages/auth/login/ui/LoginView.vue
@@ -1,3 +1,44 @@
+<template>
+  <div class="inner">
+    <h1>Вход</h1>
+    <div class="vstack">
+      <div>
+        <label class="label">Email</label>
+        <input
+          v-model="form.email"
+          :disabled="isLoading"
+          :class="{ error: hasError(form.email) }"
+          class="input" />
+      </div>
+      <div>
+        <label class="label">Пароль</label>
+        <input
+          v-model="form.password"
+          type="password"
+          :disabled="isLoading"
+          :class="{ error: hasError(form.email) }"
+          class="input" />
+      </div>
+      <div class="hstack">
+        <RouterLink
+          :to="REGISTER_LINK"
+          :disabled="isLoading"
+          :class="{ disabled: isLoading }"
+          class="btn btn-ghost">
+          Регистрация
+        </RouterLink>
+        <button
+          type="button"
+          :disabled="isLoading"
+          class="btn btn-primary"
+          @click="loginHandler">
+          Войти
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 import type { Ref } from 'vue'
 
@@ -19,10 +60,18 @@ const form: Ref<LoginForm> = ref({
 
 const router = useRouter()
 
-const loginHandler = async (): Promise<void> => {
-  if (!isFromValid(form.value)) return
+const isSubmitted: Ref<boolean> = ref(false)
+const isLoading: Ref<boolean> = ref(false)
 
+const loginHandler = async (): Promise<void> => {
+  isSubmitted.value = true
+  if (!isFromValid(form.value)) return
+  isSubmitted.value = false
+
+  isLoading.value = true
   const user = await login(form.value)
+  await new Promise((resolve) => setTimeout(resolve, 5000 * 5000))
+  isLoading.value = false
 
   if (user === null) {
     alert('Пользователь не найден!')
@@ -34,44 +83,28 @@ const loginHandler = async (): Promise<void> => {
 
 const isFromValid = (form: LoginForm): form is { email: string; password: string } =>
   Object.values(form).every(Boolean)
-</script>
 
-<template>
-  <div class="inner">
-    <h1>Вход</h1>
-    <div class="vstack">
-      <div>
-        <label class="label">Email</label>
-        <input
-          v-model="form.email"
-          class="input" />
-      </div>
-      <div>
-        <label class="label">Пароль</label>
-        <input
-          v-model="form.password"
-          type="password"
-          class="input" />
-      </div>
-      <div class="hstack">
-        <RouterLink
-          :to="REGISTER_LINK"
-          class="btn btn-ghost">
-          Регистрация
-        </RouterLink>
-        <button
-          type="button"
-          class="btn btn-primary"
-          @click="loginHandler">
-          Войти
-        </button>
-      </div>
-    </div>
-  </div>
-</template>
+const hasError = (field: string | null): boolean => isSubmitted.value && !field
+</script>
 
 <style scoped>
 .hstack {
   justify-content: space-between;
+}
+
+.error {
+  border: 1px solid red;
+}
+
+.btn:disabled {
+  opacity: 0.3;
+}
+
+.input:disabled {
+  background: gainsboro;
+}
+
+.btn-ghost.disabled {
+  pointer-events: none;
 }
 </style>
